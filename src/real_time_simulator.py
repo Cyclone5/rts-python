a34873b3e75b134cf93b38c328b2ee5d46ac563f
Cyclone_HP <ugurtumer8@gmail.com>
2024-01-04 15:55:50 +0300

    - Updated: RTS

--- src/real_time_simulator.py
+++ src/real_time_simulator.py
@@ -1,25 +1,32 @@
 # TODO LIST
-# - Pythonic şekilde yazılacak.
-# - Pacerda bulunan stateler override edilmeye uygun yazılacak. (def haline getirilecek)
+# - Previous statelerı kayıt edelim.
+# - real time'ın process kısmında geçen süreye başlangıçta gelen serial süresi hesaba katılacak mı?
+# 02.01.2024
+# - @properties eklenecek single valueysa 0'ıncı olan eğer listse listenin tamamını döndüreceğiz.
+# - iterasyonlar arası kod yazabileceği bir yapı haline getireceğim while yerine for koyup istediği kadar ilerletip sonra bekletme yapısı
+# @TODO serial connection elapsed time
+# for debug mode logging libary
 
-from timer_tic_toc import TIMER_TIC_TOC as timer
+from timertictoc import TimerTicToc as Timer
 from serial import Serial
 from typing import Optional
 from enum import Enum
 import time
 
-timerForLooping = timer()  # to compute elapsed time for a process.
-timerForWholeProcess = timer()  # to compute elapsed time for all state machine algorithm.
+timerForLooping = Timer()  # to compute elapsed time for a process.
+timerForWholeProcess = Timer()  # to compute elapsed time for all state machine algorithm.
+
 
 # define the necessary states for state-machine pacer
-INIT = 0
-PROCESS = 1
-UPDATE = 2
-WAIT = 3
-FINISH = 4
-END = 5
-OVERRUN = 6
-SERIAL = 7
+class PacerState(int, Enum):
+    INIT = 0
+    PROCESS = 1
+    UPDATE = 2
+    WAIT = 3
+    FINISH = 4
+    END = 5
+    OVERRUN = 6
+    SERIAL = 7
 
 
 class SerialStore(int, Enum):
@@ -53,178 +60,244 @@ class SerialStore(int, Enum):
 #        Smaller sampling frequency
 # ---------------------------------------------------------
 
-class REAL_TIME_PACER:
+class RealTimePacer:
     # Variable          Definition                                             Defaults
     # ==============    ==================================================     ============================
     # externalFunction  => process function as an argument of the class.
-    # SimulationTime    => defines the simulation duration in seconds.         (Default = 1 seconds)
+    # SimulationTime    => defines the simulaStion duration in seconds.         (Default = 1 seconds)
     # samplingTime      => defines the sampling period for solver in seconds.  (Default = 0.01 seconds)
     # =====================================================================================================
-    def __init__(self, externalFunction,
-                 simulationTime=1,
-                 samplingTime=1e-2,
+    def __init__(self,
+                 external_function,
+                 simulation_time=1,
+                 sampling_time=1e-2,
                  serial_port=None,
                  serial_baudrate=9600,
                  serial_delay=0.1,
                  serial_timeout=15,
-                 serial_value_mode: Optional[SerialStore] = SerialStore.SINGLE_VALUE):
+                 serial_value_mode: Optional[SerialStore] = SerialStore.SINGLE_VALUE,
+                 serial_read_wish="SERIAL_READ"
+                 ):
 
         self.SoftwareVersion = "1.0.0"  # manipulate only when stable versions are changed!
 
-        if simulationTime <= 0:
-            print("Simulation time must be positive!")
-            return
+        if simulation_time <= 0:
+            raise ValueError("Simulation time must be positive!")
 
-        if samplingTime <= 0:
-            print("Sampling period must be positive!")
+        if sampling_time <= 0:
+            raise ValueError("Sampling time must be positive!")
 
+        # Serial Init
+
+        self.serial_timeout = serial_timeout
         self.serial_connection: Optional[Serial] = None
         if serial_port is not None:
-            self.serial_connection = Serial(serial_port, serial_baudrate)
+            self.serial_connection = Serial(serial_port, serial_baudrate, timeout=self.serial_timeout)
 
         self.serial_value_mode = serial_value_mode
         self.serial_values = []
-        self.serial_timeout = serial_timeout
-        self.serial_delay = serial_delay
+        # self.serial_delay = serial_delay
+        self.serial_read_wish = self._take_bytes(serial_read_wish)
+
+        # Pacer History
+        self.pacer_history = PacerHistory()
 
         # assign the externally defined process function to self object.
-        self.processFunction = externalFunction
-        self.simulationTime = simulationTime
-        self.samplingTime = samplingTime
-        self.INITIALIZATION = INIT
-        self.PROCESS = PROCESS
-        self.UPDATE = UPDATE
-        self.WAIT = WAIT
-        self.FINISH = FINISH
-        self.OVERRUN = OVERRUN
-        self.SERIAL = SERIAL
+        self.processFunction = external_function
+        self.simulationTime = simulation_time
+        self.samplingTime = sampling_time
+        self.INITIALIZATION = PacerState.INIT
+        self.PROCESS = PacerState.PROCESS
+        self.UPDATE = PacerState.UPDATE
+        self.WAIT = PacerState.WAIT
+        self.FINISH = PacerState.FINISH
+        self.OVERRUN = PacerState.OVERRUN
+        self.SERIAL = PacerState.SERIAL
 
-        # Initialization phase starts
-        STATE = INIT
+        self.sampleLength = self.simulationTime / self.samplingTime
+        # initialize the timer vars
+        self.processCounter = 0  # defines the iteration counter
+        self.elapsedTime = 0  # defines the elapsed time during the process.
+        self.waitTime = 0  # defines the remaining time to reach the sampling time.
+        self.totalRunTime = 0  # defines the total executed time (wait time is not included)
+        self.totalSimulatedTime = 0  # defines the total simulation time including NOPs.
+        self.overrunCounter = 0  # defines the total iteration number where the overrun occurs.
+        self.overrunCases = 0  # defines the how many loops are over-runed
 
     def getSoftwareVersion(self):  # to get sofware version
         return self.SoftwareVersion
 
-    def Pacer(self, STATE):
+    def Pacer(self, state):
 
-        match STATE:
+        match state:
 
             case self.INITIALIZATION:
-                # compute the necessary iterion number to complete the simulation
-                self.sampleLength = self.simulationTime / self.samplingTime
-                # initialize the timer vars
-                self.processCounter = 0  # defines the iteration counter
-                self.elapsedTime = 0  # defines the elapsed time during the process.
-                self.waitTime = 0  # defines the remaining time to reach the sampling time.
-                self.totalRunTime = 0  # defines the total executed time (wait time is not included)
-                self.totalSimulatedTime = 0  # defines the total simulation time including NOPs.
-                self.overrunCounter = 0  # defines the total iteration number where the overrun occurs.
-                self.overrunCases = 0  # defines the how many loops are over-runed
-
-                STATE = PROCESS  # start the process.
-                timerForWholeProcess.tic()  # start the timer after initialization is performed.
-                return STATE
+                return self.pacer_init()
 
             case self.PROCESS:
-                # start the timer
-                timerForLooping.tic()
-                # run the process function
-                self.processFunction()
-                # stop the timer
-                self.elapsedTime = timerForLooping.toc()
-                print("Elapsed Time: ", self.elapsedTime, " seconds.")
-                if self.elapsedTime <= self.samplingTime:
-                    self.processCounter += 1
-                    self.waitTime = self.samplingTime - self.elapsedTime
-                    STATE = WAIT
-                    # print("1")
-                elif self.elapsedTime > self.samplingTime:
-                    print("OVERRUN case occured!")
-                    self.overrunCases = self.elapsedTime % self.samplingTime  # how many cycle is passed in an ovverrun case
-                    self.overrunCounter += 1
-                    # overrunCounter ve processCounter sayımı hakkında bilgim yok. https://prnt.sc/gwf0Igmq6ELq
-                    STATE = OVERRUN
-                    # print("2")
-                return STATE
-
-            case self.UPDATE:
-                # update the simulation related parameters
-                self.totalRunTime = self.totalRunTime + self.elapsedTime  # total run time is integrated.
-                # Parameters are updated. Detect if the simulation is over
-
-                # if simulation is over
-                if self.processCounter >= self.sampleLength:
-                    STATE = FINISH
-                else:
-                    # if simulation should continue
-                    STATE = PROCESS
-                return STATE
+                return self.pacer_process()
 
             case self.WAIT:
-                # print(str(self.waitTime) + " koyduğum bu değer wait time")
-                # @TODO: do not forget to take precautions for OVERRUN cases.
-                # For that cases, update the necessary iteration number by an appropriate number
-                # i.e. if the execution time is 0.245 seconds, and sampling time is 0.1 seconds,
-                # program should wait 0.055 seconds for the next iteration, and elpasedIteration
-                # should be increased by 3 = ceil((executionTime / samplingTime)),
-                # and return a warning message
-                timerForLooping.delay(self.waitTime)  # wait for the t(n+1) - t(n) is elapsed.
-                # wait process is done!, now it is time to update simulation parameters
-                STATE = UPDATE
-                return STATE
+                return self.pacer_wait()
 
             case self.OVERRUN:
-                self.waitTime = self.samplingTime - (self.elapsedTime % self.samplingTime)
+                return self.pacer_overrun()
 
-                # process counter is increased (-1 is employed to remove the increment n PROCESS state).
-                # self.processCounter += self.overrunCases - 1
-                self.processCounter += (self.elapsedTime // self.samplingTime) + 1
-
-                # define the state.
-                STATE = WAIT
-
-                return STATE
+            case self.UPDATE:
+                return self.pacer_update()
 
             case self.SERIAL:
-                if self.serial_connection is None:
-                    raise RuntimeError("Serial connection is not initialized!")
-                if self.serial_connection.is_open is False:
-                    raise RuntimeError("Serial connection is not open!")
-                start_time = time.time()
-                while True:
-                    if self.serial_connection.in_waiting > 0:
-                        data = self.serial_connection.readline().decode().strip()
-                        if self.serial_value_mode is SerialStore.LIST:
-                            self.serial_values.append(data)
-                        elif self.serial_value_mode is SerialStore.SINGLE_VALUE:
-                            self.serial_values.clear()
-                            self.serial_values.append(data)
-                        break
-                    if time.time() - start_time > self.serial_timeout:
-                        raise TimeoutError("Serial connection timeout! ({} seconds)".format(self.serial_timeout))
-                    time.sleep(self.serial_delay)
+                return self.pacer_serial()
 
             case self.FINISH:
-                self.totalSimulatedTime = timerForWholeProcess.toc()
-                print("============================================================================")
-                print(" => Simulation is over!")
-                # Output the simulation related parameters
-                print(" => Total simulated time: ", self.totalSimulatedTime, " seconds.")
-                print(" => Total Run Time: ", self.totalRunTime, " seconds.")
-                print(" => Total iteration that process is run: ", self.processCounter, " iterations")
-                print(" => # of iteration that Overrun detected: ", self.overrunCounter, " iterations")
-                print("============================================================================")
-                STATE = END
-                return STATE
+                return self.pacer_finish()
 
             case _:
                 pass
 
+    def pacer_init(self):
+        if self.serial_connection is None:
+            state = PacerState.PROCESS  # start the process.
+        else:
+            state = PacerState.SERIAL
+        timerForWholeProcess.tic()  # start the timer after initialization is performed.
+        return state
+
+    def pacer_process(self):
+        # start the timer
+        timerForLooping.tic()
+        # run the process function
+        self.processFunction()
+        # stop the timer
+        self.elapsedTime = timerForLooping.toc()
+        print("Elapsed Time: ", self.elapsedTime, " seconds.")
+        state = None
+        if self.elapsedTime <= self.samplingTime:
+            self.processCounter += 1
+            self.waitTime = self.samplingTime - self.elapsedTime
+            state = PacerState.WAIT
+            # print("1")
+        elif self.elapsedTime > self.samplingTime:
+            print("OVERRUN case occured!")
+            self.overrunCases = self.elapsedTime % self.samplingTime
+            # how many cycle is passed in an ovverrun case
+            self.overrunCounter += 1
+            # overrunCounter ve processCounter sayımı hakkında bilgim yok. https://prnt.sc/gwf0Igmq6ELq
+            state = PacerState.OVERRUN
+            # print("2")
+        if state is None:
+            raise RuntimeError("State is None in process!")
+        return state
+
+    def pacer_update(self):
+        # update the simulation related parameters
+        self.totalRunTime = self.totalRunTime + self.elapsedTime  # total run time is integrated.
+        # Parameters are updated. Detect if the simulation is over
+
+        # if simulation is over
+        if self.processCounter >= self.sampleLength:
+            state = PacerState.FINISH
+        else:
+            # if simulation should continue
+            if self.serial_connection is None:
+                state = PacerState.PROCESS
+            else:
+                state = PacerState.SERIAL
+        return state
+
+    def pacer_wait(self):
+        # print(str(self.waitTime) + " koyduğum bu değer wait time")
+        # @TODO: do not forget to take precautions for OVERRUN cases.
+        # For that cases, update the necessary iteration number by an appropriate number
+        # i.e. if the execution time is 0.245 seconds, and sampling time is 0.1 seconds,
+        # program should wait 0.055 seconds for the next iteration, and elpasedIteration
+        # should be increased by 3 = ceil((executionTime / samplingTime)),
+        # and return a warning message
+        timerForLooping.delay(self.waitTime)  # wait for the t(n+1) - t(n) is elapsed.
+        # wait process is done!, now it is time to update simulation parameters
+        state = PacerState.UPDATE
+        return state
+
+    def pacer_overrun(self):
+        self.waitTime = self.samplingTime - (self.elapsedTime % self.samplingTime)
+
+        # process counter is increased (-1 is employed to remove the increment n PROCESS state).
+        # self.processCounter += self.overrunCases - 1
+        self.processCounter += (self.elapsedTime // self.samplingTime) + 1
+
+        # define the state.
+        state = PacerState.WAIT
+
+        return state
+
+    def pacer_serial(self):
+        if self.serial_connection is None:
+            raise RuntimeError("Serial connection is not initialized!")
+        if self.serial_connection.is_open is False:
+            raise RuntimeError("Serial connection is not open!")
+        self.serial_connection.write(self.serial_read_wish)
+        data = self.serial_connection.readline()
+        if data == b'':
+            raise TimeoutError("Serial connection timeout! ({} seconds)".format(self.serial_timeout))
+        if self.serial_value_mode is SerialStore.LIST:
+            self.serial_values.append(data)
+        elif self.serial_value_mode is SerialStore.SINGLE_VALUE:
+            self.serial_values.clear()
+            self.serial_values.append(data)
+        # time.sleep(self.serial_delay)
+
+        state = PacerState.PROCESS
+        return state
+
+    def pacer_finish(self):
+        self.totalSimulatedTime = timerForWholeProcess.toc()
+        print("============================================================================")
+        print(" => Simulation is over!")
+        # Output the simulation related parameters
+        print(" => Total simulated time: ", self.totalSimulatedTime, " seconds.")
+        print(" => Total Run Time: ", self.totalRunTime, " seconds.")
+        print(" => Total iteration that process is run: ", self.processCounter, " iterations")
+        print(" => # of iteration that Overrun detected: ", self.overrunCounter, " iterations")
+        print("============================================================================")
+        print(self.serial_values)
+        state = PacerState.END
+        return state
+
+    def append_pacer_history(self, state: PacerState):
+        self.pacer_history.append(state)
+
+    @staticmethod
+    def _take_bytes(data):
+        if isinstance(data, str):
+            return bytes(data, 'utf-8')
+        elif isinstance(data, int):
+            return data.to_bytes(2, byteorder='big')
+
     def pacerDriver(self):
         # this function is employed to run pacer state machine.
-        stateInLoop = INIT
+        state_in_loop = PacerState.INIT
         while True:
-
-            stateInLoop = self.Pacer(stateInLoop)  # loop the paceer with state
-            if stateInLoop is END:
+            state_in_loop = self.Pacer(state_in_loop)  # loop the paceer with state
+            if state_in_loop is PacerState.END:
                 break  # leave the pacer.
+
+
+class PacerHistory:
+
+    def __init__(self):
+        self.histories = []
+
+    def append(self, state: PacerState):
+        self.histories.append(state)
+
+    def __str__(self):
+        if not self.histories:
+            return "Histories are empty."
+        else:
+            history_str = "Pacer History:\n"
+            for index, state in enumerate(self.histories, start=1):
+                history_str += f"Step {index}: {state.state}\n"
+            return history_str
+
+    def __call__(self, *args, **kwargs):
+        pass
